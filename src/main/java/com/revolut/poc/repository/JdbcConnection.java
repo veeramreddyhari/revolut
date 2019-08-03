b4b9cbdd93fc5e5c220e4a63d31baf4a2b6e1aae
Hari <veeramreddyhari@gmail.com>
2019-08-03 04:56:54 +0100

    DB properties changed

--- src/main/java/com/revolut/poc/repository/JdbcConnection.java
+++ src/main/java/com/revolut/poc/repository/JdbcConnection.java
@@ -22,7 +22,7 @@ public class JdbcConnection {
 
         Connection con = null;
         Class.forName("com.mysql.jdbc.Driver");
-        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/revolut", "root", "Natiomal13$");
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/revolut", "admin", "admin123!!!");
         return con;
 
     }
