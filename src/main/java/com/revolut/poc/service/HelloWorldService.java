946abe28505c708ea2ebb1cef42dd3079c75f8de
Hari <veeramreddyhari@gmail.com>
2019-08-05 16:02:29 +0100

    Final Version

--- src/main/java/com/revolut/poc/service/HelloWorldService.java
+++ src/main/java/com/revolut/poc/service/HelloWorldService.java
@@ -9,7 +9,11 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.Date;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 
 @Path("/user")
 public class HelloWorldService {
@@ -24,21 +28,28 @@ public class HelloWorldService {
     @Produces(MediaType.APPLICATION_JSON)
     public Response persistUser(@PathParam("name") String name, User user) {
         User oldUser = null;
+        String message = null;
         try {
-            if (user.getName() == null) {
+            if (name == null) {
                 throw new WebApplicationException(Response.Status.NO_CONTENT);
-            }
-            oldUser = userService.getUserByName(name);
+            } else if (name != null && Pattern.matches("[a-zA-Z]+", name)) {
+                user.setName(name);
+                message = validateUser(user);
+                oldUser = userService.getUserByName(name);
 
-            if (oldUser != null) {
-                userService.updateUser(user, oldUser.getId());
+                if (oldUser != null) {
+                    userService.updateUser(user, oldUser.getId());
+                } else {
+                    userService.addUser(user);
+                }
             } else {
-                userService.addUser(user);
+                message = name + " must contains only letters !";
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return Response.ok().entity(user).build();
+        return Response.ok().entity(message).type("text/plain").build();
     }
 
     @GET
@@ -67,17 +78,37 @@ public class HelloWorldService {
     private String processUser(User user) {
 
         String message = null;
-        long diffInMillies = 0l;
-        long diff = 0l;
+        Integer diff = null;
         Date current = new java.sql.Date(System.currentTimeMillis());
 
         if (DateUtil.isSameDay(current, user.getDob())) {
             message = "Hello, " + user.getName() + "!" + " Happy birthday !";
         } else {
-            diffInMillies = Math.abs(current.getTime() - user.getDob().getTime());
-            diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
-            message = "Hello, " + user.getName() + "!" + " Your birthday is in " + diff + " days(s)";
+            Long daysInDiff = fetchDays(current.toLocalDate(), user.getDob().toLocalDate());
+            message = "Hello, " + user.getName() + "!" + " Your birthday is in " + daysInDiff + " days(s)";
         }
         return message;
+    }
+
+    private String validateUser(User user) {
+        String message = null;
+        Date current = new java.sql.Date(System.currentTimeMillis());
+        if (user.getDob() != null) {
+            if (!user.getDob().before(current)) {
+                message = "Date of birth must be a date before the today date!";
+            }
+        }
+        return message;
+    }
+
+    private Long fetchDays(LocalDate currentDate, LocalDate birthDay) {
+        Long days = 0l;
+        LocalDate nextBDay = birthDay.withYear(currentDate.getYear());
+
+        if (nextBDay.isBefore(currentDate) || nextBDay.isEqual(currentDate)) {
+            nextBDay = nextBDay.plusYears(1);
+        }
+        days = ChronoUnit.DAYS.between(currentDate, nextBDay);
+        return days;
     }
 }
